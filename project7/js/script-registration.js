c4beb3fdc071e9cec86db035493e0b3403063954
ViktoriaVyshedko <147328308+ViktoriaVyshedko@users.noreply.github.com>
2024-06-13 11:38:46 +0500

    Разметка + стили + скрипты

---
+++ project7/js/script-registration.js
@@ -0,0 +1,8 @@
+let hiddenPart = document.querySelector('.hidden');
+let nextButton = document.querySelector('.create-profile');
+
+nextButton.onclick = function () {
+    hiddenPart.classList.remove('hidden');
+};
+
+
---
+++ project7/js/script-scroll.js
@@ -0,0 +1,10 @@
+let hiddenPart = document.querySelector('.hidden');
+let filterButton = document.querySelector('.filter-button');
+
+filterButton.onclick = function () {
+    hiddenPart.classList.remove('hidden');
+};
+
+
+
+
